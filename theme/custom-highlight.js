d8b7d20ac6947c8cf1e6719d352f7dac3dc830bb
Chris <filmee24@gmail.com>
2022-11-12 14:14:11 +0100

    Add do Keyword To Highlighting

--- theme/custom-highlight.js
+++ theme/custom-highlight.js
@@ -2,7 +2,7 @@ hljs.registerLanguage("back", (hljs) => ({
   name: "Backlang",
   keywords: {
     keyword:
-      "inline this const typeof global in continue return where type mut import module using as struct class interface let prop get set implement of for switch case break when if else match with while static operator private protected public internal abstract override func macro constructor destructor enum union bitfield default sizeof and or",
+      "inline this const typeof global in continue return where type mut import module using as struct class interface let prop get set implement of for switch case break when if else match with do while static operator private protected public internal abstract override func macro constructor destructor enum union bitfield default sizeof and or",
     built_in:
       "bool char string obj f16 f32 f64 u8 u16 u32 u64 i8 i16 i32 i64 none",
     literal: "false true",
